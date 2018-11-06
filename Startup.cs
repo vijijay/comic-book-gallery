3beb922cbe3c080fcfd34324dcad0c0fad41728f
Viji <mailvijijay@gmail.com>
2018-11-06 18:26:49 -0500

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comic_book_gallery.Startup))]
+namespace comic_book_gallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
