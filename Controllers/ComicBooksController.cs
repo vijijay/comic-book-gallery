f19bc1d09c319102018192ae5e4a75889093d9ac
Viji <mailvijijay@gmail.com>
2018-11-08 18:24:32 -0500

    Adding changes

---
+++ Controllers/ComicBooksController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace comic_book_gallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            ViewBag.seriesTitle = "The amazing spiderman";
+            ViewBag.issuenumber = 700;
+            ViewBag.description = "<p> Final issue! Witness the final hour</p>";
+            ViewBag.artists = new string[]
+        {
+
+            "Script: Dan Slott",
+            "Pencils: Humberto Ramos",
+            "Inks: Victoria Olazaba",
+            "Colors: Edgar Delgado",
+            "Letters: Chris Eliopoulos"
+        };
+            return View();
+        }
+
+    }
+}
